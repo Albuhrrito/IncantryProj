8ebcdb45398033059e142f8ee24d6986df72f66c
Albert Youssef <albertm.youssef@gmail.com>
2021-11-05 02:56:47 -0700

    updates

---
+++ Items/Placeable/TalciteOre.cs
@@ -0,0 +1,24 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+namespace Incantry.Items.Placeable
+{
+    public class TalciteOre : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.name = "Talcite Ore";
+            item.width = 12;
+            item.height = 12;
+            item.toolTip = "This is a modded ore";
+            item.useTime = 20;
+            item.useAnimation = 20;
+            item.useStyle = 1;
+            item.value = 50;
+            item.rare = 2;
+            item.UseSound = SoundID.Item1;
+            item.autoReuse = true;
+            item.consumable = true;
+            item.createTile = mod.TileType("TalciteOreTile");
+        }
+    }
+}
--- Items/Weapons/BasicSword.cs
+++ Items/Weapons/BasicSword.cs
@@ -1,7 +1,7 @@
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace Incantry.Items
+namespace Incantry.Items.Weapons
 {
 	public class BasicSword : ModItem
 	{
